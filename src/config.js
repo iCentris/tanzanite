4afb5c78d2b94d71fcf65072682149695ad915d3
Andy Schuler <andy@solutionx.com>
2018-10-23 15:02:52 -0600

    updated readme and default configs

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 const config = {
   auth: {
-    auth_url: "http://172.21.21.190:8080/http://172.21.21.190:4000/auth/v1/api",
     login_url: "/login?return_url={return_url}",
-    //auth_url: "https://avon.prd.vibeoffice.com/auth/v1/api"
+    auth_url: "http://localhost:4000/auth/v1/api"
   }
 };
 
