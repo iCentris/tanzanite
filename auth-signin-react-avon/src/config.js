082e6fb923869a43954c7a9a98b77e54f9595a91
Andy Schuler <shoeman22@users.noreply.github.com>
2018-12-20 11:27:27 -0700

    Update config.js

--- auth-signin-react-avon/src/config.js
+++ auth-signin-react-avon/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   auth: {
     loginUrl: '/login?return_url={return_url}',
-    authUrl: 'https://cors-anywhere.herokuapp.com/https://avon.tst.vibeoffice.com/auth/v1/api'
+    authUrl: 'https://avon.tst.vibeoffice.com/auth/v1/api'
   }
 }
 
